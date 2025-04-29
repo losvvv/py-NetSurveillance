bd915cc28566f34d6f1cfcaa747837fa4df0ff56
losvvv <losvvv@yahoo.com>
2025-04-29 11:03:31 +0300

    Fix syntax errors in python3

--- cam.py
+++ cam.py
@@ -8,16 +8,15 @@ class ChinaCam():
         self.ip = ip
         self.port = port
         self.passwd = passwd
-	if not self.passwd:
-		self.passwd='tlJwpbo6'
-	self.command_codes = {
-		"KeepAlive": 1006,
-		"ConfigSet": 1040,
-		"ConfigGet": 1042,
-		"EncodeCapability": 1360,
-		"SystemFunction": 1360
-
-	}
+        if not self.passwd:
+            self.passwd='tlJwpbo6'
+        self.command_codes = {
+            "KeepAlive": 1006,
+            "ConfigSet": 1040,
+            "ConfigGet": 1042,
+            "EncodeCapability": 1360,
+            "SystemFunction": 1360
+        }
         self.commands={
             'SystemFunction':{'strPrep':'00000002000000000050053a000000','length':5454},
             'SystemInfo': {'strPrep': '0000000d0000000000fc0336000000', 'length': 664},
@@ -46,21 +45,21 @@ class ChinaCam():
         self.conn.send(dataAuth.decode('hex'))
         time.sleep(0.1)
         dataAuthRet = self.conn.recv(300)
-	self.countPacketsSend=1
+        self.countPacketsSend=1
         sessid=repr(dataAuthRet)
         sessid=re.findall(r'"SessionID" : ".+"', sessid)[0]
         self.sessid=sessid.split(':')[1].replace(' "','').replace('"','')
         self.forpreset=self.sessid.split('x')[1]
 
     def build_packet(self, input_data, message_code, encoding="ascii"):
-	data=input_data
-	unknown_block_0 = b"\x00\x00\x00"
-	sequence_number = chr(self.countPacketsSend)
-	unknown_block_1 = b"\x00\x00\x00\x00\x00"
-	message_byte = struct.pack('<H', message_code)
-	data_len = struct.pack('<I', len(data) + 1)
-	retdata=(unknown_block_0 + sequence_number + unknown_block_1 + message_byte + data_len).encode('hex')
-	return retdata
+        data=input_data
+        unknown_block_0 = b"\x00\x00\x00"
+        sequence_number = chr(self.countPacketsSend)
+        unknown_block_1 = b"\x00\x00\x00\x00\x00"
+        message_byte = struct.pack('<H', message_code)
+        data_len = struct.pack('<I', len(data) + 1)
+        retdata=(unknown_block_0 + sequence_number + unknown_block_1 + message_byte + data_len).encode('hex')
+        return retdata
 
     def getCommList(self):
         return self.commands.keys()
@@ -79,28 +78,29 @@ class ChinaCam():
             cmdSendPr=command
             if cusDopName:
                 cmdSendPr=command+cusDopName
-	    packetSend='ff'+self.forpreset+self.commands[command]['strPrep']+('{ "Name" : "'+cmdSendPr+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }').encode('hex')+'0a'
+            packetSend='ff'+self.forpreset+self.commands[command]['strPrep']+('{ "Name" : "'+cmdSendPr+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }').encode('hex')+'0a'
         elif cusPrep:
-	    cmdSend='{ "Name" : "'+command+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }'
-	    if message_code==1042:
-		conf=self.command_codes['ConfigGet']
-	    else:
-		conf=self.command_codes['ConfigSet']
-	    if buildPacket:
-		cusPrep=self.build_packet(cmdSend,conf)		
+            cmdSend='{ "Name" : "'+command+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }'
+            if message_code==1042:
+                conf=self.command_codes['ConfigGet']
+            else:
+                conf=self.command_codes['ConfigSet']
+            if buildPacket:
+                cusPrep=self.build_packet(cmdSend,conf)
             packetSend='ff'+self.forpreset+cusPrep+('{ "Name" : "'+command+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }').encode('hex')+'0a'
-	    #print(packetSend)
+            #print(packetSend)
         elif not command in self.commands.keys() and cusPrep==False:
             cmdSendPr=command
             if cusDopName:
                 cmdSendPr=command+cusDopName
-	    cmdSend='{ "Name" : "'+cmdSendPr+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }'
-	    if message_code==1042:
-		conf=self.command_codes['ConfigGet']
-	    else:
-		conf=self.command_codes['ConfigSet']
+            cmdSend='{ "Name" : "'+cmdSendPr+'",'+cusDopParam+' "SessionID" : "'+sessid+'" }'
+            if message_code==1042:
+                conf=self.command_codes['ConfigGet']
+            else:
+                conf=self.command_codes['ConfigSet']
             packetSend='ff'+self.forpreset+self.build_packet(cmdSend,conf)+(cmdSend).encode('hex')+'0a'
-	self.countPacketsSend += 1
+
+        self.countPacketsSend += 1
         self.conn.send(packetSend.decode('hex'))
         time.sleep(0.1)
         if noWait:
@@ -130,7 +130,6 @@ class ChinaCam():
         time.sleep(0.5)
         self.sendCommand('OPMachine',True,cusPrep,' "OPMachine" : { "Action" : "Reboot" },',noWait=True)
         self.closeConnect()
-
 
     def DHCP(self,interface='eth0',state=False):
         getDhcpState=self.sendCommand('NetWork.NetDHCP')
@@ -165,7 +164,7 @@ class ChinaCam():
         return False
 
     def RovertNet(self,val):
-	return val[6]+val[7]+val[4]+val[5]+val[2]+val[3]+val[0]+val[1]
+        return val[6]+val[7]+val[4]+val[5]+val[2]+val[3]+val[0]+val[1]
 
     def Network(self,ip=False,mask=False,gateway=False,httport=80):
         getNet=self.sendCommand('NetWork.NetCommon')
@@ -183,29 +182,29 @@ class ChinaCam():
                 getNet['NetWork.NetCommon'][cfg]=ip
                 changeState=True
             if cfg=='Submask' and getNet['NetWork.NetCommon'][cfg]!=mask and mask:
-		print("Change NETMASK")
+                print("Change NETMASK")
                 getNet['NetWork.NetCommon'][cfg]=mask
                 changeState=True
             if cfg=='GateWay' and getNet['NetWork.NetCommon'][cfg]!=gateway and gateway:
-		print("Change gateway")
+                print("Change gateway")
                 getNet['NetWork.NetCommon'][cfg]=gateway
                 changeState=True
             if cfg=='HttpPort' and getNet['NetWork.NetCommon'][cfg]!=httport and httport:
-		print("Change http port")
+                print("Change http port")
                 getNet['NetWork.NetCommon'][cfg]=httport
                 changeState=True
         del getNet['Ret']
         del getNet['Name']
         del getNet['SessionID']
         if changeState:
-	    self.DHCP();
+            self.DHCP()
             self.sendCommand('NetWork.IPAdaptive', True, '000000290000000000100467000000',' "NetWork.IPAdaptive": { "IPAdaptive": false },',buildPacket=True)
             time.sleep(0.2)
             self.sendCommand('NetWork.OnvifPwdCheckout', True, '000000ea0000000000100470000000',' "NetWork.OnvifPwdCheckout": { "Enable" : false },',buildPacket=True)
             time.sleep(0.2)
             #print(' ' + json.dumps(getNet, sort_keys=True)[1:-3].replace(': ',' : ').replace( '" : [{', '" : [ {').replace('}, {', ' }, { ') + ' } ],')
             print ("Change network settings")
-	    time.sleep(0.2)
+            time.sleep(0.2)
             result = self.sendCommand('NetWork.NetCommon', True, cusPrep,' ' + json.dumps(getNet, sort_keys=True)[1:-2].replace(': ',' : ').replace(' {"G', ' { "G').replace( '" : [{', '" : [ {').replace('}, {', ' }, { ') + ' },',buildPacket=True,noWait=True,message_code=1040)
             print(result)
         else:
@@ -243,7 +242,7 @@ if __name__ == "__main__":
         elif opt in ("-l", "--list"):
             listSh=True
     if command=='' and not listSh:
-        print('You need inpute command for help use -h')
+        print('You need input the command for help use -h')
         sys.exit(2)
     cam = ChinaCam(ip,port,passwd)
     if listSh:
@@ -274,30 +273,30 @@ if __name__ == "__main__":
             cam.closeConnect()
             sys.exit(2)
     elif command=='network':
-	if not options:
-	    print('No needed options for Network example:`-c network -o 192.168.1.10,255.255.255.0,192.168.1.1`')
+        if not options:
+            print('No needed options for Network example:`-c network -o 192.168.1.10,255.255.255.0,192.168.1.1`')
             cam.closeConnect()
             sys.exit(2)
-	options=options.split(',')
-	ip=False
-	mask=False
-	gateway=False
-	httport=False
+        options=options.split(',')
+        ip=False
+        mask=False
+        gateway=False
+        httport=False
         if len(options)==4:
-		ip=options[0]
-		mask=options[1]
-		gateway=options[2]
-		httport=options[3]
+            ip=options[0]
+            mask=options[1]
+            gateway=options[2]
+            httport=options[3]
         elif len(options)==3:
-		ip=options[0]
-		mask=options[1]
-		gateway=options[2]
+            ip=options[0]
+            mask=options[1]
+            gateway=options[2]
         elif len(options)==2:
-		ip=options[0]
-		mask=options[1]
+            ip=options[0]
+            mask=options[1]
         else:
-		ip=options
-	cam.Network(ip,mask,gateway,httport)
+            ip=options
+        cam.Network(ip,mask,gateway,httport)
 
     elif command=='Record':
         options = options.split(',')
